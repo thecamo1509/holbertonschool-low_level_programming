7e06937b7c013f3d7865c39b2a84abc41643d848
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-18 20:35:43 +0000

    Correcting the 4th file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
- 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
 {
   char str[60];
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
- }
+  strcpy(str, "\"Programming is like building a multilingual puzzle");
+  puts(str);
+  return(0);
+}

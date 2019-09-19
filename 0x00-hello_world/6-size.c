764dc1025516b8cbeb44b8158bdc752ce76f7eed
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-18 22:20:12 -0500

    Adding the 6th file again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ main()
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int : %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %1u byte(s)\n", sizeof(float));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }

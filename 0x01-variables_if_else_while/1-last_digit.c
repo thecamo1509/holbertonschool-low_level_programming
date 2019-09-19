83511769628ce43c4e8852b025888fa81518411d
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-19 18:34:49 +0000

    Adding the first file with the first digit exercise

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ int main(void)
 
 	lastnumber = n % 10;
 
-	if (lastnumber > 5)
+	if (lastnumber < 6 && lastnumber != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnumber);
+	}
+	else if (lastnumber > 5)
 	{
 	printf("Last digit of %d is  and %d is greater than 5\n", n, lastnumber);
 	}
-	else if (lastnumber == 0)
-	{
-	printf("Last digit of %d is %d and is 0\n", n, lastnumber);
-	}
 	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnumber);
+	printf("Last digit of %d is %d and is 0\n", n, lastnumber);
 	}
 	return (0);
 }

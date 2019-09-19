fd1bb0535ee93b60ab8f8eb87e33c18c79878ddc
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-19 18:42:18 +0000

    Trying to add the 1590 operator

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/* more headers goes there*/
+
+/*betty style doc for function main goes there*/
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n, lastnumber;
+	int n;
+	int lastnumber;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	/*your code goes there*/
 	lastnumber = n % 10;
-
 	if (lastnumber < 6 && lastnumber != 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnumber);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, lastnumber);
 	}
 	else if (lastnumber > 5)
 	{
-	printf("Last digit of %d is  and %d is greater than 5\n", n, lastnumber);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastnumber);
 	}
 	else
-	{
-	printf("Last digit of %d is %d and is 0\n", n, lastnumber);
-	}
+		printf("Last digit of %d is %d and is 0\n", n, lastnumber);
 	return (0);
 }

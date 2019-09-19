7d15271c11c7d3a8bc69e803cc45cbca042379bd
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-19 20:23:51 +0000

    Making a counter

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <=9; c++)
+	{
+		printf("%i", c);
+	}
+	putchar('\n');
+	return (0);
+}

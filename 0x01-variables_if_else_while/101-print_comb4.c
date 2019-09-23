53bbc053d6d54f200179a0749172d06bd10fb600
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-23 16:48:51 +0000

    Adding the 12th

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*Headers*/
+
+/*main function*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = (a + 1); b <= '9'; b++)
+		{
+			for (c = (b + 1); c <= '9'; c++)
+			{
+			putchar(a);
+			putchar(b);
+			putchar(c);
+			if (a < '7' || b < '8' || c < '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

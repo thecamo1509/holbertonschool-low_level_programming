6d4dac63a1b4cbf721de01fd3501142d790675b0
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-24 17:00:57 +0000

    3rd exercise

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,20 @@
 #include <ctype.h>
 /**
  * _islower - Is a function to detect lowercases
- * c: is the parameter that is being tested
+ * @c: is the parameter that is being tested
  * Return: 1 if it is lowercase
  */
 int _islower(int c)
 {
 	int ans;
 
-	ans = (islower(c) > 0 ? 1 : 0);
+	if (c >= 'a' && c <= 'z')
+	{
+	ans = 1;
+	}
+	else
+	{
+	ans = 0;
+	}
 	return (ans);
 }

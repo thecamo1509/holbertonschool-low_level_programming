a6c786ff6bb8dcea32910d46b4bf7c3d04d8cce0
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-24 17:18:12 +0000

    Adding the 4th exercise

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+/**
+ * _isalpha - Is a function to detect lowercases
+ * @c: is the parameter that is being tested
+ * Return: 1 if it is letter
+ */
+int _isalpha(int c)
+{	
+	int ans;
+
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+	ans = 1;
+	}
+	else
+	{
+	ans = 0;
+	}
+	return (ans);
+}

225ad70d9826813fb9ad01e31214ea23ecbd534c
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-25 01:41:33 +0000

    Adding the file and updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - is a function to set a result as return
+ * @a: This is the first parameter to be tested
+ * @b: This is the second parameter to be tested
+ * Return: the absolute value of an int number
+ */
+int add(int a, int b)
+{
+	int ans;
+
+	ans = a + b;
+	return (ans);
+}

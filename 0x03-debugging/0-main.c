ed811486d9c90e1fb6bbb561935a41701191cc30
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-25 03:12:26 +0000

    Adding the first main to debug the function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* main - tests function that checks if the function works with 0
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 

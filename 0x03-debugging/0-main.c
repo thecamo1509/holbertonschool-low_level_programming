4cfc9b22351ec02f8fd69a0d198a848e0c650090
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-25 03:04:45 +0000

    Adding the first main to debug the function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* main - tests function that checks if the function works with 0
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}

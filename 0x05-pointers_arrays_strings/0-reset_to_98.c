9ea858dfe474c1793735557abfb1118ed69f559e
Camilo Morales <camiloandres.1509@gmail.com>
2019-09-30 21:36:04 +0000

    Uploading the first exercise

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* reset_to_98 - This function takes a pointer to an int parameter
+* @n: is the parameter
+* Return: no return
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

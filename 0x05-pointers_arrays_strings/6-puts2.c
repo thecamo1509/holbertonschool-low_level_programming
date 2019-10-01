f08b2e865bac3341f3840acaf115b788ae7257a2
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-01 20:23:52 +0000

    Updating again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,16 +9,16 @@ void puts2(char *str)
 {
 	int i;
 
-	_putchar(*str);
-	while (*str != '\0')
-	{
-	i++;
-	str++;
+	i = 0;
 
-		if (i % 2  == 0 && i < *str)
+	while (str[i] != '\0')
+	{
+
+		if (i % 2  == 0)
 		{
-		_putchar(*str);
+		_putchar(str[i]);
 		}
+	i++;
 	}
 _putchar('\n');
 }

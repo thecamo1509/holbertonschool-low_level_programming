40d6b2e030c37743165d718fc675d923c7765d0c
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-01 18:13:53 +0000

    Updating the 4th file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* print_rev - Function to print in reverse
+* @s: parameter
+* Return: no return
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != ('\0'))
+	{
+	s++;
+	++i;
+	}
+	for (i = i; i >= 0; i--)
+	{
+	_putchar(*s);
+	s--;
+	}
+	_putchar('\n');
+}

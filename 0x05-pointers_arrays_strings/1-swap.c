a7db1e446cfb1c2b3fefddc559af8fc9bce452ef
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-01 14:19:34 +0000

    Uploading first exercise

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
 * swap_int - Swapping values
-* @*a: first parameter
-* @*b: second parameter
+* @a: first parameter
+* @b: second parameter
 */
 
 void swap_int(int *a, int *b)

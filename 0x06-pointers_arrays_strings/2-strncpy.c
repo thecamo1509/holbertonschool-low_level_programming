f85c74f6a475d58f4ca04241c86a0fadfad029a0
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-03 19:11:33 +0000

    Updating 2th task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* _strncpy - This function will copy a string
+* @dest: Destiny string
+* @src: source string
+* @n: number of n positions
+* Return: will return the dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (dest[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}

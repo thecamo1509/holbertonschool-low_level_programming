a56de10bd88073e7b1b90eb52a60c80ad252c4ba
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-03 19:49:52 +0000

    Updating the 2nd exercise

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+iinclude "holberton.h"
 /**
 * _strncpy - This function will copy a string
 * @dest: Destiny string
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (dest[i] != '\0' && i < n)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;

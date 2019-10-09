1b7ad577397cbf9bf1ffaebc407213253e784ca6
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-09 14:37:07 -0500

    Making corrections

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * _strlen_recursion - Counts the lenght of a string
  * @s: The string to take in mind
@@ -6,10 +7,11 @@
  */
 
 int _strlen_recursion(char *s)
-{	
+{
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
-	return (1 + _strlen_recursion (s+ 1));
+
+	return (1 + _strlen_recursion(s + 1));
 }

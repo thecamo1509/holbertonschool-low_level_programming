bd9c90e4f26d7ae7b67d111076169e09af1a4134
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-09 09:37:05 -0500

    Adding the first puts function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - Function to prints a string
+ * @s: String to be printed
+ * Return: no return
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+	
+		_putchar(s[i]);
+		i++;
+		s++;
+		_puts_recursion(s);
+	}
+	else if (s[i] == '\0')
+	{
+		_putchar('\n');
+	}
+}

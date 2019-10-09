f45d809a8bf91334a63e28fddb2ec976b7d711a3
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-09 14:41:51 -0500

    Uploading 4th exercise

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - Returns value x to the power of y
+ * x: the base it
+ * y: the power of x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return x*_pow_recursion(x,y - 1);
+}

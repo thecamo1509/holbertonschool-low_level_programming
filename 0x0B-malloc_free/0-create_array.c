76d5f1ae063456955287ac8fadce7f62b4a9233d
Holberton <holberton@Holbertons-iMac.local>
2019-10-15 16:28:44 -0500

    Adding a conditional

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	if (size == 0)
+	p = malloc(sizeof(char) * size);
+	if (size == 0 || p == NULL)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;

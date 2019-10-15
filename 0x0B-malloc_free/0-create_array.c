922fa67d462adc7b81bfaf6118422a21f5ea3f54
Holberton <holberton@Holbertons-iMac.local>
2019-10-15 16:12:47 -0500

    Fixing betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,21 +1,25 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-* 
-*/
+ * create_array - This is a function to create arrays of chars
+ * @size: is the size of the array
+ * @c: is the array
+ * Return: It will return Null if the size is 0 or the p
+ */
+
 char *create_array(unsigned int size, char c)
 {
-    char *p;
-    unsigned int i;
+	char *p;
+	unsigned int i;
 
-    if (size == 0 || size == NULL)
-    {
-        return (NULL);
-    }
-    p = malloc(sizeof(char) * size);
-    for (i = 0; i < size; i++)
-    {
-        p[i] = c;
-    }
-    return (p);
+	if (size == 0 || size == NULL)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
 }

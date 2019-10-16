e895bbefbecb5c3d46e56ff239f74a3126b7b3d1
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-15 23:27:48 -0500

    Updating 3rd exercise

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Will return two a 2 pointer
+ * @width: The width of the array
+ * @height: the height of the array
+ * Return: Will return a pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **dest;
+	int i, j, o, p;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	dest = malloc(sizeof(int *) * height));
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		dest[i] = malloc(sizeof(int) * width);
+			if (dest[i])
+			{
+				for (j = i; j >= 0; j--)
+				{
+					free(dest[j]);
+				}
+				free(dest);
+			}
+	}
+	for (o = 0; o < height; o++)
+	{
+		for (p = 0; p < width; p++)
+		{
+		dest[o][p] = 0;
+		}
+	}
+	return (dest);
+}

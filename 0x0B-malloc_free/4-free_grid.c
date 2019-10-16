c56cb1f0591487d278e7cf78c6a7fc33df2d548c
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-15 23:42:03 -0500

    Freeing the previos malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * free_grid - This will frees the memory
+ * @grid: This is the grid from before
+ * @height: Is the height of the grid
+ * Return: No return
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

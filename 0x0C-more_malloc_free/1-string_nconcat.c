d9adc8d5f7f6a6b2c2e8521cf968f7459b66765e
Holberton <holberton@Holbertons-iMac.local>
2019-10-17 11:02:29 -0500

    Updating the first exercise

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _strlen - This will count the number of characters from a string
+* @s: Parameter
+* Return: Will return the number of char
+*/
+
+int _strlen(char *s)
+{
+	char *a = s;
+
+	while (*a)
+	{
+	++a;
+	}
+return (a - s);
+}
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: First String
+ * @s2: Second String
+ * @n: Number of bytes of s2
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int tam1, tam2, i, j;
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	tam1 = _strlen(s1);
+	tam2 = 0;
+	while (tam2 <= n)
+	{
+		tam2++;
+	}
+	p = malloc(sizeof(char) * (tam1 + tam2 + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < tam1; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (j = 0; j < tam2; j++)
+		{
+			p[i + j] = s2[j];
+		}
+	}
+	return (p);
+}

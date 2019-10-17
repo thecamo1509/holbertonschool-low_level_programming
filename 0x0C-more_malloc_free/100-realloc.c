1c02b78bed7cc00f964bb80938282d9f5a37c758
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-17 15:33:04 -0500

    Fixing the realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _realloc - This will reallocate a memory block
+ * @ptr: Pointer to a memory previously allocated
+ * @old_size: Size in bytes of the allocated space for ptr
+ * @new_size: New size in bytes of the new memory block
+ * Return: No return
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	unsigned int i;
+	char *ptrcpy;
+	char *dest;
+
+	ptr = malloc(old_size);
+	new = malloc(new_size);
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		return (new);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+	}
+	ptrcpy = ptr;
+	dest = new;
+
+	for (i = 0; i < new_size; i++)
+	{
+	dest[i] = ptrcpy[i];
+	}
+	free(ptr);
+	return (new);
+}

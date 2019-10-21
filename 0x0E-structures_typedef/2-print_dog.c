e50ec1ea5abb3d1a4f6293a6ee031efdaad36761
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-21 13:41:57 -0500

    Updating the second exercise

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)");
-		if (d->age == NULL)
+		if (d->age == 0)
 			printf("Age: (nil)");
 		if (d->owner == NULL)
 			printf("Owner: (nil)");

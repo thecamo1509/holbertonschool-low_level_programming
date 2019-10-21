400b3a6df145cddb094c30543012a31e4d57331c
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-21 14:40:22 -0500

    Updating the second one

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,7 @@ void print_dog(struct dog *d)
 		dueno = ((*d).owner != NULL ? (*d).owner : "(nil)");
 
 			printf("Name: %s\n", nombre);
-			if (d->age)
-			{
 			printf("Age: %f\n", d->age);
-			}
 			printf("Owner: %s\n", dueno);
 	}
 }

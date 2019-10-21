af95b1ff6886e4e67cd2d914ff71dffd27a994cf
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-21 15:29:29 -0500

    Updating the 4th exercise

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,18 +10,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
-	char *nombre;
-	char *dueno;
-
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
 	ptr->name = name;
 	ptr->owner = owner;
 	ptr->age = age;
-
-	nombre = name;
-	dueno = owner;
 
 	return (ptr);
 }

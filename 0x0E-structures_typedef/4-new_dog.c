f7ce04c7952f0d1e1177c636a23fa6c7367f49f9
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-21 17:37:54 -0500

    Updating the 4th

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,43 @@
+/**
+* _strlen - This will count the number of characters from a string
+* @s: Parameter
+* Return: Will return the number of char
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+return (i);
+}
+/**
+* _strncpy - This function will copy a string
+* @dest: Destiny string
+* @src: source string
+* @n: number of n positions
+* Return: will return the dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
 #include "dog.h"
 #include <stdlib.h>
 /**
@@ -11,21 +51,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	int i, j;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
+		return (NULL);
+
+	i = _strlen(name);
+	j = _strlen(owner);
+	ptr->name = malloc(i + 1);
+
+	if (ptr->name == NULL)
+		{
 		free(ptr);
 		return (NULL);
-	if (name == NULL)
-		ptr->name = "";
-	else
-		ptr->name = name;
-	if (owner == NULL)
-		ptr->owner = "";
-	else
-	ptr->owner = owner;
-
+		}
+	ptr->owner = malloc(j + 1);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+	ptr->name = _strncpy(ptr->name, name, i + 1);
+	ptr->owner = _strncpy(ptr->owner, owner, j + 1);
 	ptr->age = age;
-
 	return (ptr);
 }

3e46ef4f0d215636adb1e3cd3a236a94b5229005
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-23 10:29:01 -0500

    Updating the header and adding the first exercise

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - This will execute a function given as a parameter
+ * @size: size of the array
+ * @action: Pointer to the function to use
+ * @array: The array given
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
+typedef unsigned long size_t;
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

c30f2263fbc0d86fe640c38d38441057f602de9f
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-23 09:58:16 -0500

    Adding the new project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - This function will print the name
+ * @name: parameter to print
+ * @f: Parameter that points to the printingname function
+ * Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

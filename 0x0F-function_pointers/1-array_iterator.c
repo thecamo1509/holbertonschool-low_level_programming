5ce10bf17a9ab48fc2ddb1c61a2d45bbfeb04b62
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-23 12:43:36 -0500

    Updating 1st exercise

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || size <= 0 || action == NULL)
 		return;
 
-	unsigned int i = 0;
+	size_t i = 0;
 
 	while (i < size)
 	{

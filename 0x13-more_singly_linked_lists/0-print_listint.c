77f831fe9812046f6704f546c7e8928762831468
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-05 09:19:34 -0500

    Adding the readme and the first exercise

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - This function will print a list of elements
+ * @h: This is the list
+ * Return: Will return the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}

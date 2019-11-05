54ca91083485e3336d9531ef25093684f3231f09
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-05 10:59:36 -0500

    Updating the 6th

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - Deleting a node
+ * @head: Head is the starting node
+ * Return: No return
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *var;
+
+	if (head == NULL)
+	return (0);
+	if (*head != NULL)
+	{
+		i = (*head)->n;
+		var = *head;
+		*head = var->next;
+		free(var);
+	}
+	return (i);
+}

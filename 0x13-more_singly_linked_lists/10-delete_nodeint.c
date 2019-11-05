8ce94b9bd885660dd2266f3ab1010d51849a9ab6
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-05 15:20:44 -0500

    Updating if header is null

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *store2;
 	unsigned int i = 0;
 
+	if (store == NULL)
+	return (-1);
 	if (index == 0 && *head != NULL)
 	{
 		*head = (*head)->next;

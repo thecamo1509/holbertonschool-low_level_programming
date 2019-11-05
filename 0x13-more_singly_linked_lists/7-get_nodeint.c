0f2299e133e059d6d59d8473a2def56292c1b998
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-05 11:24:49 -0500

    Updating the 7th

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,10 +13,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 	return (NULL);
-	while (i < index)
+	if (head != NULL)
 	{
-		head = head->next;
-		i++;
+		while (i < index)
+		{
+			head = head->next;
+			i++;
+		}
 	}
 	return (head);
 }

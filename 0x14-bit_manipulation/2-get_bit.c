6546a7f869953e12f2a021b290835a12e6296d78
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-07 16:10:17 -0500

    Updating the 2nd exercise

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,9 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > 64)
 	return (-1);
-	for(i = 0; i < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		n = n >> 1;
 	}
-	return(n & 1);
+	return (n & 1);
 }

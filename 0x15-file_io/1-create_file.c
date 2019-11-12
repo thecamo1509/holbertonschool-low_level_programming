b2174f02beb97e2f6b41a5156189a99b1f6271e3
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-12 14:31:57 -0500

    Updating

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - Function to create file
+ * @filename: This is the name of the file
+ * @text_content: This is the content
+ * Return: 1 on success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (filename == NULL)
+	return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	write(fd, text_content, i);
+	if (fd == -1)
+	return (-1);
+
+	close(fd);
+	return (1);
+}

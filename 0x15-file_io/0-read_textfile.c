77c67a33b608cdcf97a475439af880dbfd04c861
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-12 14:07:02 -0500

    Updating file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t print;
-	char buff[1024];
+	char *buff;
 
 	if (filename == NULL)
 	return (0);
 
+	buff = malloc(sizeof(letters) * letters);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	return (-1);
+		return (-1);
 
 	print = read(fd, buff, letters);
 	if (print == -1)
 		return (0);
 	close(fd);
-	write(STDOUT_FILENO, buff, print);
-
+	fd = write(STDOUT_FILENO, buff, print);
+	if (fd == -1)
+		return (0);
+	free(buff);
 	return (print);
 }

cc2fa8251e7879744da2c223884f9da37e21c9b8
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-12 13:58:16 -0500

    Updating the first exercise

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * read_textfile - This function will read a text file and prints it to the POSIX
+ * read_textfile - This function will read a text file and prints
  * @filename: Name of the file
  * @letters: Amount of letters
  * Return: Actual number of letters it could read and print
@@ -22,9 +22,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	print = read(fd, buff, letters);
 	if (print == -1)
-		return(0);
-	close (fd);
-	write(STDOUT_FILENO, buff, print);	
-	
+		return (0);
+	close(fd);
+	write(STDOUT_FILENO, buff, print);
+
 	return (print);
 }

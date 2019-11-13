1f901c0f8c9507b06b51e5586eec778dfe3bf9db
Camilo Morales <camiloandres.1509@gmail.com>
2019-11-12 21:30:47 -0500

    Updating

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,11 +8,11 @@
 int main(int argc, char **argv)
 {
 	int fd, fdtmp, save = 1024, save2 = 0, closing;
-	char *buff;
+	char *buff[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY, 0600);
 	if (fd == -1)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fdtmp = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);

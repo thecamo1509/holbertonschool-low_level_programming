115a115aad7655b6a82d1c1e48fc47f42e07f8ae
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-13 15:46:19 -0500

    Updating the 5th

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Get node at specific index
+ * @head: Header to the first node
+ * @index: Index to go through
+ * Return: The address of the desired node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *temp = head;
+
+	if (head == NULL)
+		return (NULL);
+	while(i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return(temp);
+}

ac940d4bd9d2ed7e47e8cf967ff7b5b69f578a2f
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-14 12:16:12 -0500

    Correcting betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,11 +9,11 @@ size_t dlistint_len(const dlistint_t *h)
 	size_t i = 0;
 
 	if (h == NULL)
-		return(0);
-	while(h)
+		return (0);
+	while (h)
 	{
 		h = h->next;
 		i++;
 	}
-	return(i);
+	return (i);
 }

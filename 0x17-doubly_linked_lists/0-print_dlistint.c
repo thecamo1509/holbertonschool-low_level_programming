eb3340be395d475f8c4f8934561cf990660071d5
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-14 12:11:38 -0500

    Updating the 0 file with betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,16 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
+
 	if (h == NULL)
 	{
-		return(0);
+		return (0);
 	}
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
-	return(i);
+	return (i);
 }

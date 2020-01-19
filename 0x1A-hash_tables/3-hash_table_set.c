7df06ca4629e5b821ee18140b8fa1044b4f6c8a5
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-18 23:41:22 -0500

    Adding a free

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,6 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[idx] && strcmp(key, ht->array[idx]->key) == 0)
 	{
+		free(ht->array[idx]->value);
 		ht->array[idx]->value = strdup(value);
 		return (1);
 	}
